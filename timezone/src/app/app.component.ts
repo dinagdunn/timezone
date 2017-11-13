9bb60fe00114e76d6e6f7af1819d1a468e095d07
dinagdunn <dunn.dina@gmail.com>
2017-11-12 21:30:23 -0800

    first commit

---
+++ timezone/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent{
+time = new Date();
+lastclicked = null;
+
+updateTimeZone(timezone) {
+  this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+    }
+    else if (timezone === 'PST') {
+      this.time = new Date();
+    }  
+    else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+    }  
+    else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+    }
+    this.lastclicked = timezone;
+  }
+}
